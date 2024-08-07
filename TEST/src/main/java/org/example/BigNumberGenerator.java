84cc4c49e8d0a3fbd2ea82687f046cdef2cfd97b
Szymon <szafraniecszymon@gmail.com>
2024-08-07 14:40:20 +0200

    [007] BigNumberGenerator class refactor

---
+++ TEST/src/main/java/org/example/BigNumberGenerator.java
@@ -0,0 +1,37 @@
+package main.java.org.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class BigNumberGenerator {
+    private final String defaultSaveFilePath = "Test/src//sourceFiles/bigNumberAdditionTest.csv";
+    private final int decimalBase = 10;
+    public boolean generateToFile(int minDigits, int maxDigits,
+                                  int numberOfRandoms, String filePath) {
+
+        try (FileWriter writer = new FileWriter(filePath)) {
+            for (int i = 0; i < numberOfRandoms; i++) {
+                String number1 = generateRandomNumber(minDigits, maxDigits);
+                String number2 = generateRandomNumber(minDigits, maxDigits);
+                writer.append(number1).append(";").append(number2).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    private String generateRandomNumber(int minDigits, int maxDigits) {
+        Random random = new Random();
+        int numberOfDigits = random.nextInt(maxDigits - minDigits + 1) + minDigits;
+        StringBuilder number = new StringBuilder();
+
+        for(int i=0; i<numberOfDigits; i++) {
+            number.insert(0, random.nextInt(this.decimalBase));
+        }
+
+        return number.toString();
+    }
+}
--- TEST/src/main/java/org/example/Main.java
+++ TEST/src/main/java/org/example/Main.java
@@ -29,39 +29,6 @@ public class Main {
 }
 
 
-class BigNumberGenerator {
-    private final String defaultSaveFilePath = "Test/src//sourceFiles/bigNumberAdditionTest.csv";
-    private final int decimalBase = 10;
-    public boolean generateToFile(int minDigits, int maxDigits,
-                                  int numberOfRandoms, String filePath) {
-
-        try (FileWriter writer = new FileWriter(filePath)) {
-            for (int i = 0; i < numberOfRandoms; i++) {
-                String number1 = generateRandomNumber(minDigits, maxDigits);
-                String number2 = generateRandomNumber(minDigits, maxDigits);
-                writer.append(number1).append(";").append(number2).append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return true;
-    }
-
-    private String generateRandomNumber(int minDigits, int maxDigits) {
-        Random random = new Random();
-        int numberOfDigits = random.nextInt(maxDigits - minDigits + 1) + minDigits;
-        StringBuilder number = new StringBuilder();
-
-        for(int i=0; i<numberOfDigits; i++) {
-            number.insert(0, random.nextInt(this.decimalBase));
-        }
-
-        return number.toString();
-    }
-}
-
-
 @FunctionalInterface
 interface BigNumberCalculatorConsumer<T, V> {
     void accept(T t, V v);
