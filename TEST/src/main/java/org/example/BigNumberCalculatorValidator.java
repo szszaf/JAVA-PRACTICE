a9f39995cc68b621fcd963ad72b2809cd526d8be
Szymon <szafraniecszymon@gmail.com>
2024-08-07 14:39:06 +0200

    [007] BigNumberCalculatorValidator class refactor

---
+++ TEST/src/main/java/org/example/BigNumberCalculatorValidator.java
@@ -0,0 +1,41 @@
+package main.java.org.example;
+
+import java.io.*;
+
+public class BigNumberCalculatorValidator {
+
+    private final String validationFilePath = "Test/src//sourceFiles/bigNumberAdditionTest.csv";
+    private final int firstElementIndexInValidationFile = 0;
+    private final int secondElementIndexInValidationFile = 1;
+    private final int sumElementIndexInValidationFile = 2;
+    public void validationTest(BigNumberCalculator calculator) throws FileNotFoundException {
+
+        try (BufferedReader br = new BufferedReader(
+                new FileReader(new File(this.validationFilePath))
+        )) {
+            String line;
+
+            int testNO = 1;
+            while ((line = br.readLine()) != null) {
+                String []values = line.split(";");
+
+                String firstNumber = values[this.firstElementIndexInValidationFile].trim();
+                String secondNumber = values[this.secondElementIndexInValidationFile].trim();
+                String actualResult = values[this.sumElementIndexInValidationFile].trim();
+                String result = calculator.addWithChunks(firstNumber, secondNumber);
+
+                if(result.equals(actualResult)) {
+                    System.out.println("Test " + testNO + " passed");
+                } else {
+                    System.out.println("Test " + testNO + " not passed");
+                    System.out.println(actualResult);
+                    System.out.println(result);
+                }
+                System.out.println("----------------------------");
+                testNO++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- TEST/src/main/java/org/example/Main.java
+++ TEST/src/main/java/org/example/Main.java
@@ -28,49 +28,6 @@ public class Main {
     }
 }
 
-/**TEST TEST
- *
- */
-
-
-
-class BigNumberCalculatorValidator {
-
-    private final String validationFilePath = "Test/src//sourceFiles/bigNumberAdditionTest.csv";
-    private final int firstElementIndexInValidationFile = 0;
-    private final int secondElementIndexInValidationFile = 1;
-    private final int sumElementIndexInValidationFile = 2;
-    public void validationTest(BigNumberCalculator calculator) throws FileNotFoundException {
-
-        try (BufferedReader br = new BufferedReader(
-                new FileReader(new File(this.validationFilePath))
-        )) {
-            String line;
-
-            int testNO = 1;
-            while ((line = br.readLine()) != null) {
-                String []values = line.split(";");
-
-                String firstNumber = values[this.firstElementIndexInValidationFile].trim();
-                String secondNumber = values[this.secondElementIndexInValidationFile].trim();
-                String actualResult = values[this.sumElementIndexInValidationFile].trim();
-                String result = calculator.addWithChunks(firstNumber, secondNumber);
-
-                if(result.equals(actualResult)) {
-                    System.out.println("Test " + testNO + " passed");
-                } else {
-                    System.out.println("Test " + testNO + " not passed");
-                    System.out.println(actualResult);
-                    System.out.println(result);
-                }
-                System.out.println("----------------------------");
-                testNO++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
 
 class BigNumberGenerator {
     private final String defaultSaveFilePath = "Test/src//sourceFiles/bigNumberAdditionTest.csv";
